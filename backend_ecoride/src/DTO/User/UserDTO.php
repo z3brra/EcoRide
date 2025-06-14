b477e453bb817e20b69a576f1880b05bbf5c6413
z3brra <pas.tp2000@gmail.com>
2025-06-14 19:41:05 +0200

    Feature : API - Add User DTO (create / update + read)

---
+++ backend_ecoride/src/DTO/User/UserDTO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DTO\User;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class UserDTO
+{
+    #[Assert\NotBlank(message: "Pseudo is required.", groups: ['create'])]
+    #[Assert\Length(
+        min: 2,
+        minMessage: "Pseudo must have at least 2 caracters.",
+        max: 64,
+        maxMessage: "Pseudo may not exceed 64 caracters.",
+        groups: ['create', 'update']
+    )]
+    public ?string $pseudo = null;
+
+    public function isEmpty(): bool
+    {
+        return $this->pseudo === null;
+    }
+}
+
+?>
---
+++ backend_ecoride/src/DTO/User/UserReadDTO.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\DTO\User;
+
+use App\Entity\User;
+
+use DateTimeImmutable;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+class UserReadDTO
+{
+    #[Groups(['user:read', 'user:list'])]
+    public string $uuid;
+
+    #[Groups(['user:read', 'user:list'])]
+    public string $pseudo;
+
+    #[Groups(['user:read', 'user:list'])]
+    public string $email;
+
+    #[Groups(['user:read', 'user:list'])]
+    public array $roles;
+
+    #[Groups(['user:read', 'user:list'])]
+    public ?int $credits;
+
+    #[Groups(['user:read', 'user:list'])]
+    public bool $isBanned;
+
+    #[Groups(['user:read'])]
+    public DateTimeImmutable $createdAt;
+
+    #[Groups(['user:read'])]
+    public ?DateTimeImmutable $updatedAt;
+
+    #[Groups(['user:read'])]
+    public string $apiToken;
+
+    public function __construct(
+        string $uuid,
+        string $pseudo,
+        string $email,
+        array $roles,
+        ?int $credits = null,
+        bool $isBanned,
+        DateTimeImmutable $createdAt,
+        ?DateTimeImmutable $updatedAt = null,
+        string $apiToken,
+    )
+    {
+        $this->uuid = $uuid;
+        $this->pseudo = $pseudo;
+        $this->email = $email;
+        $this->roles = $roles;
+        $this->credits = $credits;
+        $this->isBanned = $isBanned;
+        $this->createdAt = $createdAt;
+        $this->updatedAt = $updatedAt;
+        $this->apiToken = $apiToken;
+    }
+
+    public static function fromEntity(User $user): self
+    {
+        $userDTO = new self(
+            uuid: $user->getUuid(),
+            pseudo: $user->getPseudo(),
+            email: $user->getEmail(),
+            roles: $user->getRoles(),
+            credits: $user->getCredits(),
+            isBanned: $user->isBanned(),
+            createdAt: $user->getCreatedAt(),
+            updatedAt: $user->getUpdatedAt(),
+            apiToken: $user->getApiToken()
+        );
+
+        return $userDTO;
+    }
+}
+
+?>
