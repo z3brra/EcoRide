48f53cd305c65c3913b6d0a58ec43d0acdc01396
z3brra <pas.tp2000@gmail.com>
2025-06-16 09:54:35 +0200

    Feature : API - Add ban and unban user endpoint for Admin

--- backend_ecoride/src/Controller/AdminController.php
+++ backend_ecoride/src/Controller/AdminController.php
@@ -4,7 +4,8 @@ namespace App\Controller;
 
 use App\DTO\User\{UserDTO, UserReadDTO};
 use App\Service\Admin\{
-    CreateUserService
+    CreateUserService,
+    BanUserService
 };
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,7 +14,7 @@ use Symfony\Component\HttpFoundation\{Request, JsonResponse};
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
-use Symfony\Component\HttpKernel\Exception\{BadRequestHttpException, ConflictHttpException};
+use Symfony\Component\HttpKernel\Exception\{NotFoundHttpException, BadRequestHttpException, ConflictHttpException};
 use Symfony\Component\Serializer\Annotation\Context;
 
 #[Route('/api/admin', name: 'app_api_admin_')]
@@ -63,6 +64,68 @@ final class AdminController extends AbstractController
             return new JsonResponse(
                 data: ['error' => $e->getMessage()],
                 status: JsonResponse::HTTP_CONFLICT
+            );
+        }
+    }
+
+    #[Route('/ban-user', name: 'ban_user', methods: 'POST')]
+    public function banUser(
+        Request $request,
+        BanUserService $banUserService
+    ): JsonResponse {
+        try {
+            $userUuid = $request->query->get('userUuid', null);
+            if (!$userUuid) {
+                throw new BadRequestHttpException("userUuid is required");
+            }
+
+            $banUserService->banUser($userUuid);
+
+            return new JsonResponse(
+                data: ['message' => 'User successfully banned'],
+                status: JsonResponse::HTTP_OK
+            );
+
+        } catch (NotFoundHttpException $e) {
+            return new JsonResponse(
+                data: ['error' => $e->getMessage()],
+                status: JsonResponse::HTTP_NOT_FOUND
+            );
+        } catch (BadRequestHttpException $e) {
+            return new JsonResponse(
+                data: ['error' => $e->getMessage()],
+                status: JsonResponse::HTTP_BAD_REQUEST
+            );
+        }
+    }
+
+    #[Route('/unban-user', name: 'unban_user', methods: 'POST')]
+    public function unbanUser(
+        Request $request,
+        BanUserService $banUserService
+    ): JsonResponse {
+        try {
+            $userUuid = $request->query->get('userUuid', null);
+            if (!$userUuid) {
+                throw new BadRequestHttpException("userUuid is required");
+            }
+
+            $banUserService->unbanUser($userUuid);
+
+            return new JsonResponse(
+                data: ['message' => 'User successfully unbanned'],
+                status: JsonResponse::HTTP_OK
+            );
+
+        } catch (NotFoundHttpException $e) {
+            return new JsonResponse(
+                data: ['error' => $e->getMessage()],
+                status: JsonResponse::HTTP_NOT_FOUND
+            );
+        } catch (BadRequestHttpException $e) {
+            return new JsonResponse(
+                data: ['error' => $e->getMessage()],
+                status: JsonResponse::HTTP_BAD_REQUEST
             );
         }
     }
